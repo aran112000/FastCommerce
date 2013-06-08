fe53fcd16b1d37871fa328159692e00413cca050
Aran <cdtreeks@hotmail.com>
2013-06-08 23:10:09 +0100

    Begging to style up a front-end

--- inc/theme/buyshop/index.php
+++ inc/theme/buyshop/index.php
@@ -1,25 +1,16 @@
 <?=$this->get_html_header()?>
 <header>
 	<a href="/" title="<?=get::setting('site_name')?>">
-		<img src="<?=$this->di->theme_class->get_path('/images/logo.png')?>" width="300" height="100" />
-		<h1><?=get::setting('site_name')?></h1>
+		<img src="<?=$this->di->theme_class->get_path('/images/logo.png')?>" width="250" height="85" />
 	</a>
+	<?=$this->di->pages->get_nav('main', array('where' => 'nav=1'))?>
 </header>
 <section id="main">
 	<article id="content">
 		<?=$this->get_html_content()?>
 	</article>
-	<aside id="left">
-		<p>Leftcol</p>
-		<p>
-			<?
-			$prod = new prod();
-			echo '<p><pre>' . print_r($prod->do_retrieve_from_id(array(), 1), true) . '</pre></p>'."\n";
-			?>
-		</p>
-	</aside>
-	<aside id="right">
-		<p>Rightcol</p>
-	</aside>
+	<footer>
+		<small>&copy; <?=date('Y') . ' ' . get::setting('site_name')?></small>
+	</footer>
 </section>
 <?=$this->get_html_footer()?>
