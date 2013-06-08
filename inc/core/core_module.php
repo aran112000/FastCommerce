7300be98a775971fddf63cf62028b6abf3444e04
Aran <cdtreeks@hotmail.com>
2013-06-08 20:45:55 +0100

    Further testing carried out on aliased modules (from the URL), to make life easier core_module now has $this->$fn_path_number to specify at which point in the URL parts the module FN should be (zero based) - Default is 1, page module currently is 0 although page URL structure may change to /PID/FN as it could create too many reserved words in the future TBC
    get_view() updated to not use $this->table to look for the view files as if aliased then it won't exist, instead we now use get_called_class()

--- inc/core/core_module.php
+++ inc/core/core_module.php
@@ -5,6 +5,13 @@
 class core_module extends seo {
 
 	/**
+	 * The zero based number at which the item fn should appear within the module URL
+	 *
+	 * @var int
+	 */
+	public $fn_path_number = 1;
+
+	/**
 	 * @param null $table
 	 */
 	public function __construct($table = NULL) {
@@ -23,24 +30,18 @@ class core_module extends seo {
 	 * @return bool|string
 	 */
 	public function __controller($path_parts, $path_count) {
-		if ($path_count > 1) {
-			run::header_redir('/' . $path_parts[0], 301);
-		} else {
-			if ($path_parts[0] == 404) {
-				// TODO - These need to be noindex,follow
-				run::http_status(404);
-				return $this->get_view('404');
-			} else {
-				if (!$this->current = $this->do_retrieve(array(), array('where' => 'fn=:fn', 'params' => array('fn' => $path_parts[0])))) {
-					run::header_redir('/404', 404);
-				}
-
-				parent::__controller($path_parts, $path_count);
-
-				return $this->get_view('default');
-			}
+		if ($path_parts[0] == 404) {
+			run::http_status(404);
+			return $this->get_view('404');
 		}
 
+		if (!$this->current = $this->do_retrieve(array(), array('where' => 'fn=:fn', 'params' => array('fn' => $path_parts[$this->fn_path_number])))) {
+			run::header_redir('/404', 404);
+		}
+
+		parent::__controller($path_parts, $path_count);
+
+		return $this->get_view('default');
 	}
 
 	/**
@@ -49,9 +50,10 @@ class core_module extends seo {
 	 * @return bool|string
 	 */
 	public function get_view($view = 'default') {
-		if (is_readable(root . '/inc/module/' . $this->table . '/view/' . $view . '.php')) {
+		$module_folder = get_called_class();
+		if (is_readable(root . '/inc/module/' . $module_folder . '/view/' . $view . '.php')) {
 			ob_start();
-			require(root . '/inc/module/' . $this->table . '/view/' . $view . '.php');
+			require(root . '/inc/module/' . $module_folder . '/view/' . $view . '.php');
 			$html = ob_get_contents();
 			ob_end_clean();
 
