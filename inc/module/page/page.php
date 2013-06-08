0d26d01724b8db3c35cf24dcfad880516a891089
Aran <cdtreeks@hotmail.com>
2013-06-08 20:46:52 +0100

    Page controller updated and default path number for FN ($this->fn_path_number) updated from new default of 1 to 0

--- inc/module/page/page.php
+++ inc/module/page/page.php
@@ -3,4 +3,23 @@
  * Class page
  */
 final class page extends core_module {
+
+	/**
+	 * @param string $table
+	 */
+	public function __construct($table = 'page') {
+		$this->fn_path_number = 0;
+		parent::__construct($table);
+	}
+
+	/**
+	 * @param $path_parts
+	 * @param $path_count
+	 */
+	public function __controller($path_parts, $path_count) {
+		if ($path_count > 1) {
+			run::header_redir('/' . $path_parts[0], 301);
+		}
+		return parent::__controller($path_parts, $path_count);
+	}
 }
