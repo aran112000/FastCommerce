36c9af7823728e3c0c054e215a860a7493d15859
Aran <cdtreeks@hotmail.com>
2013-06-08 20:47:39 +0100

    Added __construct() extend to supply the correct table name (due to aliasing) to allow the table definition to be correctly retrieved.

--- inc/module/product/product.php
+++ inc/module/product/product.php
@@ -3,4 +3,11 @@
  * Class product
  */
 final class product extends core_module {
+
+	/**
+	 * @param string $table
+	 */
+	public function __construct($table = NULL) {
+		parent::__construct('prod');
+	}
 }
