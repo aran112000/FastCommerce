086bffb38514fcda69e6f48c59ef3d3d7f07279c
Aran <cdtreeks@hotmail.com>
2013-06-08 20:17:38 +0100

    - added __controller() functionality to allow pages to exist and map up to modules automatically based on the first uri part from $_SERVER['REQUEST_URI'] e.g. /prod/test will go to a prod module if it exists else it would fallback to look for a page with fn 'prod' - Trailing elements are removed from pages via a 301 redirect - 404 pages now exists - moved from the root of /inc into /inc/core along with other key files

--- inc/core/core.php
+++ inc/core/core.php
@@ -1,8 +1,21 @@
 <?
+/*
+ * TODOs
+ *  - More core into DI when loaded for any items such as SEO tags can go directly in from any object or module
+ * */
+
+
 /**
  * Class core
  */
 final class core {
+
+	/**
+	 * Holds all the page details required by the main theme (/theme/[theme_name]/index.php) to display the site content
+	 *
+	 * @var array
+	 */
+	private $page = array();
 
 	/**
 	 * Constructor
@@ -12,7 +25,33 @@ final class core {
 			$this->di = new di();
 		}
 		if (ajax) {
+			// TODO - Create ajax handler in PHP & JS
+		} else {
+			$this->__controller();
+		}
+	}
 
+	public function __controller() {
+		$url_parts = $this->get_url_parts();
+		if (!empty($url_parts)) {
+			$module_name = $url_parts[0];
+			$module_aliases = get::conf('module_aliases');
+			if (isset($module_aliases[$module_name])) {
+				$module_name = $module_aliases[$module_name];
+			}
+
+			if (is_readable(root . '/inc/module/' . $module_name . '/' . $module_name . '.php')) {
+				// TODO test this block
+				${$module_name} = new $module_name($module_name);
+				if (get::method_exists(${$module_name}, '__controller')) {
+					$this->page['body'] = ${$module_name}->__controller($url_parts, count($url_parts));
+				} else {
+					trigger_error('No controller found for module: ' . $module_name);
+				}
+			} else {
+				$page = new page('page');
+				$this->page['body'] = $page->__controller($url_parts, count($url_parts));
+			}
 		}
 	}
 
@@ -35,6 +74,13 @@ final class core {
 	}
 
 	/**
+	 * @return array
+	 */
+	private function get_url_parts() {
+		return explode('/', substr(uri, 1, strlen(uri)));
+	}
+
+	/**
 	 * @return string
 	 */
 	public function get_html_header() {
@@ -53,7 +99,7 @@ final class core {
 	 * @return string
 	 */
 	public function get_html_content() {
-		return ''; // TODO
+		return (isset($this->page['body']) ? $this->page['body'] : '');
 	}
 
 	/**
