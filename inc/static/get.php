185794e03f8694a669cb35c4a73ded3a2f803b08
Aran <cdtreeks@hotmail.com>
2013-06-08 19:07:55 +0100

    For efficiency, wrappers created for class_exists() and method_exists() for savings should they be called multiple times per page load on the same classes (caches responses in static) - Methods get::class_exists() & get::method_exists()

--- inc/static/get.php
+++ inc/static/get.php
@@ -15,18 +15,34 @@ final class get {
 	private static $fetched_settings = array();
 
 	/**
+	 * @var array
+	 */
+	private static $method_exists_cache = array();
+
+	/**
+	 * @var array
+	 */
+	private static $class_exists_cache = array();
+
+	/**
 	 * @param string $block
 	 * @param string $key
 	 * @param string $fallback_value
 	 * @return string
 	 */
-	public static function conf($block, $key, $fallback_value = '') {
+	public static function conf($block, $key = '', $fallback_value = '') {
 		if (empty(self::$conf_file)) {
 			self::$conf_file = parse_ini_file(root . '/.conf/default.ini', true, INI_SCANNER_RAW);
 		}
 
-		if (isset(self::$conf_file[$block][$key])) {
-			return trim(self::$conf_file[$block][$key]);
+		if (!empty($key)) {
+			if (isset(self::$conf_file[$block][$key])) {
+				return trim(self::$conf_file[$block][$key]);
+			}
+		} else {
+			if (isset(self::$conf_file[$block])) {
+				return trim(self::$conf_file[$block]);
+			}
 		}
 
 		return trim($fallback_value);
@@ -52,5 +68,33 @@ final class get {
 		}
 
 		return trim($fallback_value);
+	}
+
+	/**
+	 * @param $object
+	 * @param $method_name
+	 *
+	 * @return mixed
+	 */
+	public static function method_exists($object, $method_name) {
+		$class_name = get_class($object);
+		if (!isset(self::$method_exists_cache[$class_name][$method_name])) {
+			self::$method_exists_cache[$class_name][$method_name] = (method_exists($object, $method_name));
+		}
+
+		return self::$method_exists_cache[$class_name][$method_name];
+	}
+
+	/**
+	 * @param $class_name
+	 *
+	 * @return mixed
+	 */
+	public static function class_exists($class_name) {
+		if (!isset(self::$method_exists_cache[$class_name])) {
+			self::$method_exists_cache[$class_name] = (class_exists($class_name));
+		}
+
+		return self::$method_exists_cache[$class_name];
 	}
 }
