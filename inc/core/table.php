f9a734609e501091f98ab9011dff78ae51638ed2
Aran <cdtreeks@hotmail.com>
2013-06-08 18:27:10 +0100

    - Refactored $this->mysql_select_ignore_fields to $this->ignore_fields - Added option $this->retrieve_nonlive (default is false) so all results will be checked against live & deleted flags (if present in the MySQL table) - Added method do_retrieve_from_id() to fetch fields from MySQL database based on a single ID (provided in argument 2) - Added method get_default_select_fields() to get the default MySQL select fields if non have been provided - Fields marked in $this->ignore_fields will be excluded, all other MySQL fields for the current table will be fetched - Currently only used by do_retrieve_from_id()

--- inc/core/table.php
+++ inc/core/table.php
@@ -10,7 +10,12 @@ class table {
 	 *
 	 * @var array
 	 */
-	public $mysql_select_ignore_fields = array('live', 'deleted');
+	public $ignore_fields = array('live', 'deleted');
+
+	/**
+	 * @var bool
+	 */
+	public $retrieve_nonlive = false;
 
 	/**
 	 * Leave NULL for to inherit DB table name from calling class, this should only be used if you need to alias
@@ -40,4 +45,61 @@ class table {
 		}
 	}
 
+	public function do_retrieve_from_id(array $fields = array(), $id, array $options = array()) {
+		if (empty($fields)) {
+			$fields = $this->get_default_select_fields();
+		}
+
+		$sql = 'SELECT `' . implode('`, `', $fields) . '` FROM `' . $this->table . '`';
+
+		$where = array(
+			'`' . key($this->fields) . '` = :id'
+		);
+		if (!$this->retrieve_nonlive) {
+			if (isset($this->fields['live'])) {
+				$where[] = 'live = 1';
+			}
+			if (isset($this->fields['deleted'])) {
+				$where[] = 'deleted = 0';
+			}
+		}
+		if (isset($options['where']) && !empty($options['where'])) {
+			if (is_array($options['where'])) {
+				$where = array_merge($where, $options['where']);
+			} else {
+				$where[] = $options['where'];
+			}
+		}
+		if (!empty($where)) {
+			$sql .= ' WHERE ' . implode(' AND ', $where);
+		}
+
+		$params = array(
+			'id' => (int) $id,
+		);
+		if (!empty($options['params']) && is_array($options['params'])) {
+			$params = array_merge($params, $options['params']);
+		}
+		$sql .= ' LIMIT 1';
+
+		$tres = db::query($sql, $params);
+		if ($tres && db::num($tres) == 1) {
+			return db::fetch_class($tres, $this->table);
+		}
+
+		return false;
+	}
+
+	private function get_default_select_fields() {
+		$fields = array();
+		if (!empty($this->fields)) {
+			foreach ($this->fields as $field => $opts) {
+				if (!in_array($field, $this->ignore_fields)) {
+					$fields[] = $field;
+				}
+			}
+		}
+
+		return $fields;
+	}
 }
