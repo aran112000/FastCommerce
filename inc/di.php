985fd1a34ed571d05c73b2f443093d0e4f68946a
Aran <cdtreeks@hotmail.com>
2013-06-08 16:11:13 +0100

    Key changes made to __get() to handle closures correctly and cache the resulting values after first use

--- inc/di.php
+++ inc/di.php
@@ -3,14 +3,20 @@ final class di {
 
 	public function __get($name) {
 		if (isset(di_container::$registrants[$name])) {
-			return di_container::$registrants[$name];
+			$val = di_container::$registrants[$name];
+			if (gettype($val) == 'object' && is_callable($val)) {
+				// Functions stored within DI aren't executed unless required to same on memory and if called again the value is simply returned
+				$val = $val();
+				di_container::$registrants[$name] = $val;
+			}
+			return $val;
 		}
 
 		throw new Exception($name . ' not set, please check isset() before attempting to fetch');
 	}
 
 	public function __set($name, $value) {
-		di_container::$registrants[$name] = $value;
+		$this->add($name, $value);
 	}
 
 	public function __isset($name) {
@@ -21,5 +27,9 @@ final class di {
 		if (isset($this->$name)) {
 			unset(di_container::$registrants[$name]);
 		}
+	}
+
+	public function add($name, $value) {
+		di_container::$registrants[$name] = $value;
 	}
 }
