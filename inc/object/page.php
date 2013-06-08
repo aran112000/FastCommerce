2b4e2f6524b2fc6ffa6dfa73eb47e67ce216189d
Aran <cdtreeks@hotmail.com>
2013-06-09 00:36:46 +0100

    get_url() now supports direct links following DB and __controller() changes

--- inc/object/page.php
+++ inc/object/page.php
@@ -8,6 +8,9 @@ final class page extends table {
 	 * @return string
 	 */
 	public function get_url() {
+		if (!empty($this->direct_link)) {
+			return $this->direct_link;
+		}
 		return '/' . ($this->pid == 1 ? '' : $this->pid . '/' . $this->fn);
 	}
 }
