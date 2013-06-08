b3a1eecbaaeb73e49c9fc458424ce717501ddcf6
Aran <cdtreeks@hotmail.com>
2013-06-08 20:19:18 +0100

    Basic structure for an SEO module to ensure all modules are as SEO friendly as possible right out the box

---
+++ inc/core/seo.php
@@ -0,0 +1,21 @@
+<?
+/**
+ * Class seo
+ *
+ * Called by default at the end of the core_module controller - Responsible for the following:
+ *  - Title Tag                                    TODO
+ *  - Meta Description                             TODO
+ *  - Meta Keywords (disabled by default)          TODO
+ *  - Robots : follow/nofollow...                  TODO
+ *  - Social tags (Facebook OG & Tweet Cards)      TODO
+ */
+class seo extends table {
+
+	/**
+	 * @param $path_parts
+	 * @param $path_count
+	 */
+	public function __controller($path_parts, $path_count) {
+		// TODO : Once core is in DI this will be easy
+	}
+}
