5f0b016cb8328d912561a90cb45d0b0dbd1300f4
Aran <cdtreeks@hotmail.com>
2013-06-09 15:28:48 +0100

    Added opencart style view markup for products, other modules still to be refactored to match this format

--- inc/module/categories/view/default.php
+++ inc/module/categories/view/default.php
@@ -1,3 +1,20 @@
-<h1><?=$this->current->title?></h1>
-<?=$this->current->body?>
-<?=$this->get_list($this->current->cid)?>
+<h1><?= $this->current->title ?></h1>
+<?= $this->current->body ?>
+<?= $this->get_list($this->current->cid) ?>
+<?
+$prods = $this->di->prod_list->get_list_from_cat($this->di->categories->current->cid);
+if ($prods && !empty($prods)) {
+	?>
+	<ul id="prod" class="list">
+		<? foreach ($prods as $prod) { ?>
+			<li>
+				<a href="<?= $prod->get_url() ?>" title="<?= $prod->price ?>">
+					<span class="padded_img"><img src="http://placehold.it/230x290" alt="<?= $prod->title ?>"
+												  width="230" height="290"/></span>
+					<strong class="title"><?= $prod->title ?></strong>
+					<strong class="price">&#163;<?= number_format($prod->price, 2) ?></strong>
+				</a>
+			</li>
+		<? } ?>
+	</ul>
+<? } ?>
