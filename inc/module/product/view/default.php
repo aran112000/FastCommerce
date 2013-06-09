786a55a26d524932d67f352ffa5484cafbbe316a
Aran <cdtreeks@hotmail.com>
2013-06-09 16:12:58 +0100

    More detailed product view created, now includes price, image, stock levels & Vimeo videos (data dependant)

--- inc/module/product/view/default.php
+++ inc/module/product/view/default.php
@@ -1,2 +1,18 @@
-<h1><?=$this->current->title?></h1>
-<?=$this->current->body?>
+<article id="prod_detail">
+	<div id="left">
+		<span class="padded_img"><img src="http://placehold.it/400x550" alt="<?=$this->current->title ?>" width="400" height="550" /></span>
+	</div>
+	<div id="right">
+		<h1><?=$this->current->title?></h1>
+		<p><strong class="price"><span class="label">Price:</span> &#163;<?=number_format($this->current->price, 2)?></strong><br />
+		<strong class="stock"><span class="label">Stock:</span> <?=($this->current->stock > 0 ? 'In stock <small>(' . $this->current->stock . ' remaining)</small>' : 'Out of stock')?></strong></p>
+		<section id="body">
+			<p><?=$this->current->body?></p>
+		</section>
+		<?if (!empty($this->current->vimeo_url)) {?>
+		<section id="video">
+			<iframe src="<?=$this->current->vimeo_url?>" width="700" height="405"></iframe>
+		</section>
+		<?}?>
+	</div>
+</article>
