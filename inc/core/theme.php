439024f67dfb77d86ecf030c67f54799edc67423
Aran <cdtreeks@hotmail.com>
2013-06-15 00:22:58 +0100

    - Theme now stores $this->base_theme_path - used caching set my new method get_theme(); - added theme->get_theme() - added theme->get_fallback_theme() - Designed to get the first theme found within $this->base_theme_path that's not the '_global' dir - set_resources() refactored to being in all JS files from _global by default

--- inc/core/theme.php
+++ inc/core/theme.php
@@ -7,7 +7,7 @@ final class theme extends dependency {
 	/**
 	 * @var string
 	 */
-	private $base_theme_path = '';
+	private $base_theme_path = '/inc/theme';
 
 	/**
 	 * @var array
@@ -15,11 +15,53 @@ final class theme extends dependency {
 	private $theme_settings = array();
 
 	/**
+	 * @var string
+	 */
+	protected $theme = '';
+
+	/**
 	 */
 	public function __init() {
-		$this->base_theme_path = '/inc/theme/' . $this->di->get->setting('theme');
+		$this->base_theme_path = $this->di->asset->get_theme_dir();
 		$this->get_theme_setting();
 		$this->set_resources();
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function get_theme() {
+		if (!empty($this->theme)) {
+			return $this->theme;
+		}
+		$theme = $this->di->get->setting('theme', 'buyshop');
+		if (!empty($theme)) {
+			$theme_dir = root . $this->di->asset->get_theme_dir();
+			if (is_readable($theme_dir . $theme . '/index.php')) {
+				$this->theme = $theme;
+				return $theme;
+			}
+		}
+
+		// TODO - Write to error tbl, no visible error should be thrown for this
+		$this->get_fallback_theme();
+		return $this->theme;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function get_fallback_theme() {
+		foreach (glob(root . $this->base_theme_path . '*', GLOB_ONLYDIR) as $dir) {
+			if (!strstr($dir, '/_global')) {
+				$theme_path_parts = explode('/', str_replace(root, '', $dir));
+				$this->theme = end($theme_path_parts);
+
+				return $this->theme;
+			}
+		}
+
+		return false;
 	}
 
 	/**
@@ -27,7 +69,7 @@ final class theme extends dependency {
 	 * @return string
 	 */
 	public function get_path($relative_path) {
-		return $this->base_theme_path . $relative_path;
+		return $this->base_theme_path . $this->theme . '/' . $relative_path;
 	}
 
 	/**
@@ -44,14 +86,18 @@ final class theme extends dependency {
 		return $this->theme_settings;
 	}
 
+	/**
+	 *
+	 */
 	private function set_resources() {
-		if (isset($this->theme_settings['global_js'])) {
-			foreach ($this->theme_settings['global_js'] as $js) {
-				$this->di->core->footer_js_files[$js] = '/inc/theme/_global/js/' . $js;
-			}
+		foreach(glob(root . '/inc/theme/_global/js/*.js') as $js) {
+			$this->di->core->footer_js_files[$js] = str_replace(root, '', $js);
+		}
+		foreach(glob(root . $this->base_theme_path . '_global/*.js') as $js) {
+			$this->di->core->footer_js_files[$js] = str_replace(root, '', $js);
 		}
 
-		foreach (glob(root . $this->base_theme_path . '/js/*.js') as $theme_js) {
+		foreach (glob(root . $this->base_theme_path . $this->get_theme() . '/js/*.js') as $theme_js) {
 			$theme_js = str_replace(root, '', $theme_js);
 			$this->di->core->footer_js_files[$theme_js] = $theme_js;
 		}
