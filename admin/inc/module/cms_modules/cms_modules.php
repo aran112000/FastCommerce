5f23d1cc8a82385b50a37a246f2ebc86972b016f
Aran <cdtreeks@gmail.com>
2013-06-18 00:47:29 +0100

    Changed get_module_list() to output all fields for the time being onto the list page.

--- admin/inc/module/cms_modules/cms_modules.php
+++ admin/inc/module/cms_modules/cms_modules.php
@@ -13,36 +13,41 @@ class cms_modules extends core_module {
 	 * @return string
 	 */
 	public function get_module_list() {
+		$html = '';
 		$items = $this->di->{$this->current->table_name}->do_retrieve();
 		if (!empty($items)) {
-			$html = '<table id="example-datatables" class="table table-striped table-bordered table-hover">'."\n";
-			$html .= '<thead>'."\n";
-				$html .= '<tr>'."\n";
-					$html .= '<th class="span1"></th>'."\n";
-					$html .= '<th class="span1 hidden-phone">#</th>'."\n";
-					$html .= '<th>Username</th>'."\n";
-					$html .= '<th>Email</th>'."\n";
-					$html .= '<th>Status</th>'."\n";
-				$html .= '</tr>'."\n";
-			$html .= '</thead>'."\n";
-			$html .= '<tbody>'."\n";
-
-				foreach ($items as $item) {
+			$html = '<table id="module_item_list" class="table table-striped table-bordered table-hover">'."\n";
+				$html .= '<thead>'."\n";
 					$html .= '<tr>'."\n";
-						$html .= '<td class="span1">'."\n";
-							$html .= '<div class="btn-group">'."\n";
-								$html .= '<a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-mini btn-success"><i class="icon-pencil"></i></a>'."\n";
-								$html .= '<a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-mini btn-danger"><i class="icon-remove"></i></a>'."\n";
-							$html .= '</div>'."\n";
-						$html .= '</td>'."\n";
-						$html .= '<td class="span1 hidden-phone">' . $item->{$this->current->primary_key} . '</td>'."\n";
-						$html .= '<td><a href="javascript:void(0)">' . $item->title . '</a></td>'."\n";
-						$html .= '<td class="hidden-phone hidden-tablet">user1@example.com</td>'."\n";
-						$html .= '<td><span class="label label-important">Unapproved</span></td>'."\n";
+						$html .= '<th class="span1"></th>'."\n";
+						$i = 0;
+						foreach ($this->di->{$this->current->table_name}->fields as $field => $opt) { $i++;
+							if ($field != 'live' && $field != 'deleted') {
+								$html .= '<th>' . ($i == 1 ? '#' : $field) . '</th>'."\n";
+							}
+						}
 					$html .= '</tr>'."\n";
-				}
-			$html .= '</tbody>'."\n";
-		$html .= '</table>'."\n";
+				$html .= '</thead>'."\n";
+				$html .= '<tbody>'."\n";
+					foreach ($items as $item) {
+						$html .= '<tr>'."\n";
+							$html .= '<td class="span1">'."\n";
+								$html .= '<div class="btn-group">'."\n";
+									$html .= '<a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-mini btn-success"><i class="icon-pencil"></i></a>'."\n";
+									$html .= '<a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-mini btn-danger"><i class="icon-remove"></i></a>'."\n";
+								$html .= '</div>'."\n";
+							$html .= '</td>'."\n";
+							foreach ($this->di->{$this->current->table_name}->fields as $field => $opt) { $i++;
+								if (isset($item->{$field})) {
+									$html .= '<td><span>' . $item->{$field} . '</span></td>'."\n";
+								}
+							}
+						$html .= '</tr>'."\n";
+					}
+				$html .= '</tbody>'."\n";
+			$html .= '</table>'."\n";
+
+			$this->di->core->inline_js['#module_item_list'] = '$(\'#module_item_list\').dataTable();';
 		}
 
 		return $html;
