25638a49dc58d751e9f5301f86a06d5736f7d988
Aran <cdtreeks@hotmail.com>
2013-06-17 23:27:39 +0100

    Although this is currently very weak, for the time being object names are the singular version of the module e.g. pages module has a page object (using the `page` DB table) - Calls new $this->di->get->singular() method

--- inc/core/dependency.php
+++ inc/core/dependency.php
@@ -16,6 +16,7 @@ class dependency {
 		$this->di = $di;
 		//if ($this->di->get->method_exists($this, '__init')) {
 		if (method_exists($this, '__init')) {
+			$this->table = $this->di->get->singular(get_called_class());
 			$this->__init();
 		}
 	}
