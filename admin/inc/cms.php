b8e609dd16d9c200c5a1737d5aa8efc725aad848
Aran <cdtreeks@hotmail.com>
2013-06-17 23:35:00 +0100

    - Removed redundant call to __init() - Added extend of new method get_module_aliases() - Fixed path to modernizer JS lib

--- admin/inc/cms.php
+++ admin/inc/cms.php
@@ -10,17 +10,10 @@ final class cms extends core {
 	public $page_prefix = '/admin/';
 
 	/**
-	 *
-	 */
-	public function __init() {
-		parent::__init();
-	}
-
-	/**
 	 * @return array
 	 */
 	public function get_url_parts() {
-		$parts =  explode('/', substr(uri, 1, strlen(uri)));
+		$parts = explode('/', substr(uri, 1, strlen(uri)));
 		if (isset($parts[0])) {
 			$this->page_prefix = '/' . $parts[0] . '/';
 			unset($parts[0]);
@@ -28,6 +21,16 @@ final class cms extends core {
 		}
 
 		return $parts;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	protected function get_module_aliases() {
+		$cms_module_aliases = array(
+			'module' => 'cms_modules',
+		);
+		return array_merge($this->di->get->conf('module_aliases'), $cms_module_aliases);
 	}
 
 	/**
@@ -48,7 +51,7 @@ final class cms extends core {
 			$html .= "\t".'<meta name="viewport" content="width=device-width,initial-scale=1">'."\n";
 			$html .= "\t".'<link rel="dns-prefetch" href="http' . (ssl ? 's' : '') . '://' . host . '" />'."\n";
 			$html .= "\t".'<title>' . (isset($this->page['title_tag']) ? $this->page['title_tag'] : '') . ' | FastCommerce Admin</title>'."\n";
-			$html .= "\t".'<script src="' . $this->page_prefix . 'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>'."\n"; // TODO change
+			$html .= "\t".'<script src="' . $this->di->theme->get_path('js/modernizr-2.6.2-respond-1.1.0.min.js') . '"></script>'."\n"; // TODO change
 		$html .= '</head>'."\n";
 		$html .= '<body>'."\n";
 
