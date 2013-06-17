e2f0899a5241cd26f961f0a2fda8e9abf853105e
Aran <cdtreeks@hotmail.com>
2013-06-17 23:26:18 +0100

    - hardcoded '/inc/module/' dir refactored to use dynamic path from $this->di->asset->get_module_dir() - Declared new var $fn_column (public) - Designed to allow the unique field in URL to be something other than an fn e.g. `table_name` used in `cms_module` for CMS URLs - Type hinting added for __controller() - Added throughout codebase

--- inc/core/core_module.php
+++ inc/core/core_module.php
@@ -12,6 +12,11 @@ class core_module extends seo {
 	public $fn_path_number = 1;
 
 	/**
+	 * @var string
+	 */
+	public $fn_column = 'fn';
+
+	/**
 	 * @var null
 	 */
 	protected $table = NULL;
@@ -41,14 +46,12 @@ class core_module extends seo {
 		}
 	}
 
-
 	/**
-	 * @param $path_parts
-	 * @param $path_count
-	 *
+	 * @param array $path_parts
+	 * @param int   $path_count
 	 * @return bool|string
 	 */
-	public function __controller($path_parts, $path_count) {
+	public function __controller(array $path_parts, $path_count) {
 		if (!isset($this->current) || empty($this->current)) {
 			$this->set_current($fn = (isset($path_parts[$this->fn_path_number]) ? $path_parts[$this->fn_path_number] : ''));
 		}
@@ -65,6 +68,7 @@ class core_module extends seo {
 	 * @return bool
 	 */
 	public function set_current($fn, array $fields = array(), array $opts = array('where' => 'fn=:fn', 'limit' => 1)) {
+		$opts['where'] = str_replace($opts['where'], 'fn=:fn', $this->fn_column . '=:fn');
 		$opts['params']['fn'] = $fn;
 		if (!$this->current = $this->di->{$this->table}->do_retrieve($fields, $opts)) {
 			$this->di->run->header_redir('/404', 404);
@@ -80,9 +84,9 @@ class core_module extends seo {
 	 */
 	public function get_view($view = 'default') {
 		$module_folder = get_called_class();
-		if (is_readable(root . '/inc/module/' . $module_folder . '/view/' . $view . '.php')) {
+		if (is_readable(root . $this->di->asset->get_module_dir() . $module_folder . '/view/' . $view . '.php')) {
 			ob_start();
-			require(root . '/inc/module/' . $module_folder . '/view/' . $view . '.php');
+			require(root . $this->di->asset->get_module_dir() . $module_folder . '/view/' . $view . '.php');
 			$html = ob_get_contents();
 			ob_end_clean();
 
