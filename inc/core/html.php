dcd413f00450464399760f9ee1e72067e5d54c5b
Aran <cdtreeks@gmail.com>
2013-06-20 23:07:43 +0100

    get_list() now tested and also provides additional functionality such as auto adding classes to the <li> elements

--- inc/core/html.php
+++ inc/core/html.php
@@ -47,13 +47,25 @@ class html extends dependency {
 	 * @param array $opts
 	 * @return string
 	 */
-	public function get_list(array $data, array $opts) {
-		if (!empty($data)) {
+	public function get_list(array $data, array $opts = array()) {
+		$tot = count($data);
+		if ($tot > 0) {
+			$i = 0;
+			$row_cnt = 0;
 			$html = '<ul' . (isset($opts['attr']) ? $this->di->get->attributes($opts['attr']) : '') . '>';
-			foreach ($data as $class => $row) {
-				$html .= "\t".'<li' . (!is_numeric($class) ? $this->di->get->attribute('class', $class) : '') . '>'."\n";
+			foreach ($data as $class => $row) { $i++; $row_cnt++;
+				$classes = array();
+				if (!is_numeric($class)) $classes[] = $class;
+				if ($i == 1) $classes[] = 'first';
+				if ($tot == $i) $classes[] = 'last';
+				if (isset($opts['cols']) && $row_cnt == $opts['cols']) {
+					$classes[] = 'end';
+					$row_cnt = 0;
+				}
+
+				$html .= "\t".'<li' . $this->di->get->attribute('class', $classes) . '>'."\n";
 				$html .= "\t\t".$row."\n";
-				$html .= "\t".'<li>'."\n";
+				$html .= "\t".'</li>'."\n";
 			}
 			$html .= '</ul>'."\n";
 
