309805e9182fee5147dc21f2c617cbb7bbae7811
Aran <cdtreeks@gmail.com>
2013-06-20 23:10:04 +0100

    Removed legacy commented code and changed $di to protected from public

--- inc/core/dependency.php
+++ inc/core/dependency.php
@@ -7,14 +7,13 @@ class dependency {
 	/**
 	 * @var di
 	 */
-	public $di;
+	protected $di;
 
 	/**
 	 * @param $di
 	 */
 	public function set_di(di &$di) {
 		$this->di = $di;
-		//if ($this->di->get->method_exists($this, '__init')) {
 		if (method_exists($this, '__init')) {
 			$this->table = $this->di->get->singular(get_called_class());
 			$this->__init();
