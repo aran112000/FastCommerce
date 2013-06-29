70c98ee59e12c401796fde135c822513579d1029
Aran <cdtreeks@gmail.com>
2013-06-29 18:29:37 +0100

    Bug fix, too much of the loading process was being excluded when !load_core

--- inc/loader.php
+++ inc/loader.php
@@ -11,34 +11,34 @@ spl_autoload_register(function($class) {
 	}
 });
 
+$di = new di();
+
+// Secondary autoloaders
+spl_autoload_register(function($class) use ($di) {
+	$object_dir = root . $di->asset->get_object_dir();
+	if (file_exists($object_dir . $class . '.php')) {
+		require($object_dir . $class . '.php');
+		return true;
+	}
+
+	$module_dir = root . $di->asset->get_module_dir();
+	if (file_exists($module_dir . $class . '/' . $class . '.php')) {
+		require($module_dir . $class . '/' . $class . '.php');
+		return true;
+	}
+
+	return false;
+});
+
+$memcached_servers = array(
+	'server1' => array($host = '127.0.0.1', $port = 11211, $weight = 100),
+);
+$di->cache->connect($memcached_servers);
+
+$di->core = $di->load_class('core');
+$di->core->set_di($di);
+
 if (!defined('load_core') || load_core) {
-	$di = new di();
-
-	// Secondary autoloaders
-	spl_autoload_register(function($class) use ($di) {
-		$object_dir = root . $di->asset->get_object_dir();
-		if (file_exists($object_dir . $class . '.php')) {
-			require($object_dir . $class . '.php');
-			return true;
-		}
-
-		$module_dir = root . $di->asset->get_module_dir();
-		if (file_exists($module_dir . $class . '/' . $class . '.php')) {
-			require($module_dir . $class . '/' . $class . '.php');
-			return true;
-		}
-
-		return false;
-	});
-
-	$memcached_servers = array(
-		'server1' => array($host = '127.0.0.1', $port = 11211, $weight = 100),
-	);
-	$di->cache->connect($memcached_servers);
-
-	$di->core = $di->load_class('core');
-	$di->core->set_di($di);
-
 	if (!ajax) {
 		$di->core->load_theme();
 	}
