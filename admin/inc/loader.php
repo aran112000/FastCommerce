a0bc4561a5bc58f9055d5eeb273dad2f014be487
Aran <cdtreeks@gmail.com>
2013-07-13 09:35:14 +0100

    DI is always loaded regardless load_core being false (we would want it if doing an import which didn't want to load core) - This was a design oversight

--- admin/inc/loader.php
+++ admin/inc/loader.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require(root . '/inc/config.php');
 define('cms', true);
 
@@ -13,32 +13,32 @@ spl_autoload_register(function($class) {
 	}
 });
 
+$di = new di();
+
+$di->asset->set_module_dir('/admin/inc/module/');
+$di->asset->set_object_dir('/admin/inc/object/');
+$di->asset->set_theme_dir('/admin/inc/theme/');
+
+// Secondary autoloaders
+spl_autoload_register(function($class) use ($di) {
+	$object_dir = root . $di->asset->get_object_dir();
+	if (file_exists($object_dir . $class . '.php')) {
+		require($object_dir . $class . '.php');
+		return true;
+	}
+
+	$module_dir = root . $di->asset->get_module_dir();
+	if (file_exists($module_dir . $class . '/' . $class . '.php')) {
+		require($module_dir . $class . '/' . $class . '.php');
+		return true;
+	}
+
+	return false;
+});
+
+$di->core = $di->load_class('cms');
+
 if (!defined('load_core') || load_core) {
-	$di = new di();
-
-	$di->asset->set_module_dir('/admin/inc/module/');
-	$di->asset->set_object_dir('/admin/inc/object/');
-	$di->asset->set_theme_dir('/admin/inc/theme/');
-
-	// Secondary autoloaders
-	spl_autoload_register(function($class) use ($di) {
-		$object_dir = root . $di->asset->get_object_dir();
-		if (file_exists($object_dir . $class . '.php')) {
-			require($object_dir . $class . '.php');
-			return true;
-		}
-
-		$module_dir = root . $di->asset->get_module_dir();
-		if (file_exists($module_dir . $class . '/' . $class . '.php')) {
-			require($module_dir . $class . '/' . $class . '.php');
-			return true;
-		}
-
-		return false;
-	});
-
-	$di->core = $di->load_class('cms');;
-
 	if (!ajax) {
 		$di->core->load_theme();
 	}
