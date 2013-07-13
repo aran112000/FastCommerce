15aceff1f911ab85419b661436f4a68e17f9e9ca
Aran <cdtreeks@gmail.com>
2013-07-13 09:55:59 +0100

    Changed from using PHP short tags for increased support

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
-<? define('root', $_SERVER['DOCUMENT_ROOT']); // Path your FastCommerce files
+<?php
+define('root', $_SERVER['DOCUMENT_ROOT']); // Path your FastCommerce files
 require(root . '/inc/loader.php');
